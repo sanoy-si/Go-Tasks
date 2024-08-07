bba559d0824c4162844a6133aaf386d6e0cf5674
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-07 12:06:23 +0300

    feat: Add register and login paths

--- Task Management System with JWT/router/router.go
+++ Task Management System with JWT/router/router.go
@@ -9,10 +9,11 @@ import (
 
 func BindRouter(r *gin.Engine, service *data.PersistentTaskManagementService){
 	r.GET("/tasks", controllers.GetTasks(service))
-	r.GET("tasks/:id", controllers.GetTask(service))
+	r.GET("/tasks/:id", controllers.GetTask(service))
 	r.POST("/tasks", controllers.CreateTask(service))
-	r.PUT("tasks/:id", controllers.UpdateTask(service))
-	r.DELETE("tasks/:id", controllers.DeleteTask(service))
-	r.POST("register", controllers.Register(service))
+	r.PUT("/tasks/:id", controllers.UpdateTask(service))
+	r.DELETE("/tasks/:id", controllers.DeleteTask(service))
+	r.POST("/register", controllers.Register(service))
+	r.POST("/login", controllers.Login(service))
 
 }
