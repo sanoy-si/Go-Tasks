dde83ec737e4abf2a24d15cae6cc2cdc928b2685
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-07 11:38:01 +0300

    feat: update the user model
    Update the user mode to have additional attributes

--- Task Management System with JWT/models/user.go
+++ Task Management System with JWT/models/user.go
@@ -11,6 +11,7 @@ type User struct {
 	FirstName string             `json:"first_name" validate:"required,min=2,max=100"`
 	LastName  string             `json:"last_name" validate:"required,min=2,max=100"`
 	Email     string             `json:"email" validate:"email,required"`
+	UserName  string			 `json:"username" validate:"required,min=5"`
 	Password  string             `json:"password" validate:"required,min=6"`
 	IsAdmin   bool               `json:"is_admin"`
 	CreatedAt time.Time          `json:"created_at"`
