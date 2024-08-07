eeea74b3b8191f00a97a7fef69bdc54fd8a86dd7
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-07 19:50:53 +0300

    feat: Add the bson tag to all of the user attributes

--- Task Management System with JWT/models/user.go
+++ Task Management System with JWT/models/user.go
@@ -8,12 +8,12 @@ import (
 
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	FirstName string             `json:"first_name" validate:"required,min=2,max=100"`
-	LastName  string             `json:"last_name" validate:"required,min=2,max=100"`
-	Email     string             `json:"email" validate:"email,required"`
-	UserName  string			 `json:"username" validate:"required,min=5"`
-	Password  string             `json:"password" validate:"required,min=6"`
-	IsAdmin   bool               `json:"is_admin"`
-	CreatedAt time.Time          `json:"created_at"`
-	UpdatedAt time.Time          `json:"updated_at"`
+	FirstName string             `bson:"first_name" json:"first_name" validate:"required,min=2,max=100"`
+	LastName  string             `bson:"last_name" json:"last_name" validate:"required,min=2,max=100"`
+	Email     string             `bson:"email" json:"email" validate:"email,required"`
+	UserName  string			 `bson:"username" json:"username" validate:"required,min=5"`
+	Password  string             `bson:"password" json:"password" validate:"required,min=6"`
+	IsAdmin   bool               `bson:"is_admin" json:"is_admin"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
