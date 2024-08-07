bda50e538a2f830be0a3331dc33e6c749bcf5266
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-07 19:49:43 +0300

    feat: Add the new endpoints to the router

--- Task Management System with JWT/router/router.go
+++ Task Management System with JWT/router/router.go
@@ -4,16 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"Task_Management_System_with_JWT/controllers"
 	"Task_Management_System_with_JWT/data"
+	"Task_Management_System_with_JWT/middleware"
+
 
 )
 
-func BindRouter(r *gin.Engine, service *data.PersistentTaskManagementService){
-	r.GET("/tasks", controllers.GetTasks(service))
-	r.GET("/tasks/:id", controllers.GetTask(service))
-	r.POST("/tasks", controllers.CreateTask(service))
-	r.PUT("/tasks/:id", controllers.UpdateTask(service))
-	r.DELETE("/tasks/:id", controllers.DeleteTask(service))
-	r.POST("/register", controllers.Register(service))
-	r.POST("/login", controllers.Login(service))
+func BindRouter(r *gin.Engine, taskService *data.PersistentTaskManagementService, userService data.UserService){
+	r.GET("/tasks", middleware.AuthMiddleware(), controllers.GetTasks(taskService))
+	r.GET("/tasks/:id", middleware.AuthMiddleware(), controllers.GetTask(taskService))
+	r.POST("/tasks", middleware.AuthMiddleware(), controllers.CreateTask(taskService))
+	r.PUT("/tasks/:id", middleware.AuthMiddleware(), controllers.UpdateTask(taskService))
+	r.DELETE("/tasks/:id", middleware.AuthMiddleware(), controllers.DeleteTask(taskService))
+	r.POST("/promote/:username", middleware.AuthMiddleware(), controllers.PromoteUser(userService))
+	r.POST("/register", controllers.Register(userService))
+	r.POST("/login", controllers.Login(userService))
 
 }
