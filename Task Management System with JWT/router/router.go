34a9d3a3a5f348a6c69350afa786fb1e18378d40
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-08 16:32:15 +0300

    feat: Add the admin middleware

--- Task Management System with JWT/router/router.go
+++ Task Management System with JWT/router/router.go
@@ -12,10 +12,10 @@ import (
 func BindRouter(r *gin.Engine, taskService *data.PersistentTaskManagementService, userService data.UserService){
 	r.GET("/tasks", middleware.AuthMiddleware(), controllers.GetTasks(taskService))
 	r.GET("/tasks/:id", middleware.AuthMiddleware(), controllers.GetTask(taskService))
-	r.POST("/tasks", middleware.AuthMiddleware(), controllers.CreateTask(taskService))
-	r.PUT("/tasks/:id", middleware.AuthMiddleware(), controllers.UpdateTask(taskService))
-	r.DELETE("/tasks/:id", middleware.AuthMiddleware(), controllers.DeleteTask(taskService))
-	r.POST("/promote/:username", middleware.AuthMiddleware(), controllers.PromoteUser(userService))
+	r.POST("/tasks", middleware.AuthMiddleware(), middleware.AdminMiddleware(),controllers.CreateTask(taskService))
+	r.PUT("/tasks/:id", middleware.AuthMiddleware(), middleware.AdminMiddleware(), controllers.UpdateTask(taskService))
+	r.DELETE("/tasks/:id", middleware.AuthMiddleware(), middleware.AdminMiddleware(), controllers.DeleteTask(taskService))
+	r.POST("/users/:username/promote", middleware.AuthMiddleware(), middleware.AdminMiddleware(), controllers.PromoteUser(userService))
 	r.POST("/register", controllers.Register(userService))
 	r.POST("/login", controllers.Login(userService))
 
