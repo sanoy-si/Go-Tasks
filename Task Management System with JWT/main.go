7f191edd2996d03136c1aba63d03e5ad315bffea
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-08 16:31:11 +0300

    feat: Add godotenv

--- Task Management System with JWT/main.go
+++ Task Management System with JWT/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"Task_Management_System_with_JWT/router"
 	"Task_Management_System_with_JWT/data"
+	"Task_Management_System_with_JWT/router"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	err := godotenv.Load(".env.template")
+	if err != nil{
+		log.Fatal("Couldn't load .env file.")
+	}
 
 	r := gin.Default()
-	taskService := data.NewPersistentTaskManagementService()
-	userService := data.NewMongoDBService()
+	taskService, err := data.NewPersistentTaskManagementService()
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	userService, err := data.NewMongoDBUserService()
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+	
 	router.BindRouter(r, taskService, userService)
 	
 	r.Run("localhost:8080")
