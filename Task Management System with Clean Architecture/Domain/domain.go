44b7c337eab287d958f58261d094e36f23563c2a
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-10 00:32:40 +0300

    feat: Update domain.go
    Added some methods to the user_repository and task_repository interfaces

--- Task Management System with Clean Architecture/Domain/domain.go
+++ Task Management System with Clean Architecture/Domain/domain.go
@@ -2,6 +2,8 @@ package domain
 
 import (
 	"time"
+	"context"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -23,4 +25,38 @@ type User struct {
 	IsAdmin   bool               `bson:"is_admin" json:"is_admin"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+}
+
+type TaskUsecase interface{
+	GetTasks() ([]Task, error)
+	GetTask(id string) (Task, error)
+	CreateTask(newTask Task) (Task, error)
+	UpdateTask(id string, updatedTask Task) (Task, error)
+	DeleteTask(id string) error
+}
+
+
+type UserUsecase interface{
+	Register(user User) (interface{}, error)
+	Login(username, password string) (string, error)
+	PromoteUser(username string) error
+}
+
+type TaskRepository interface{
+	GetTasks(ctx context.Context) ([]Task, error)
+	GetTaskByID(id string, ctx context.Context) (Task, error)
+	CreateTask(newTask Task, ctx context.Context) (Task, error)
+	UpdateTask(id string, updatedTask Task, ctx context.Context) (Task, error)
+	DeleteTask(id string, ctx context.Context) error
+	
+}
+
+type UserRepository interface{
+	Register(user User, ctx context.Context) (interface{}, error)
+	Login(username, password string, ctx context.Context) (string, error)
+	PromoteUser(username string, ctx context.Context) error
+	CountUserByUsername(username string, ctx context.Context)
+	CountUserByEmail(email string, ctx context.Context)
+	GetUserByUsername(usename string, ctx context.Context)
+	
 }
