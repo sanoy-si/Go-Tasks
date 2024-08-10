5d026931a4baaaeb7fef89ebeab000918e5c58aa
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-10 12:01:30 +0300

    feat: Update domain.go
    updated domain.go such that:
        - both models have an attribute id of string which will be generated by a helper function
        - added validation tags to the Task model's attribute
        - updated the status attribute of the task model to completed having a value of type boolean
        - updated some of the functions' return type
        - created the userCredentials struct

--- Task Management System with Clean Architecture/Domain/domain.go
+++ Task Management System with Clean Architecture/Domain/domain.go
@@ -3,33 +3,34 @@ package domain
 import (
 	"time"
 	"context"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Task struct {
-	ID          string    `json:"id"`
-	Title       string    `json:"title"`
+	ID          string    `bson:"_id"`
+	Title       string    `json:"title" validate:"required,min=2,max=30"`
 	Description string    `json:"description"`
 	DueDate     time.Time `json:"due_date"`
-	Status      string    `json:"status"`
+	Completed   bool      `json:"completed"`
 }
 
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	ID        string			 `bson:"_id"`
 	FirstName string             `bson:"first_name" json:"first_name" validate:"required,min=2,max=100"`
 	LastName  string             `bson:"last_name" json:"last_name" validate:"required,min=2,max=100"`
 	Email     string             `bson:"email" json:"email" validate:"email,required"`
 	UserName  string			 `bson:"username" json:"username" validate:"required,min=5"`
 	Password  string             `bson:"password" json:"password" validate:"required,min=6"`
-	IsAdmin   bool               `bson:"is_admin" json:"is_admin"`
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	IsAdmin   bool               `bson:"is_admin"`
+}
+
+type UserCredentials struct{
+	UserName  string			 `json:"username" validate:"required"`
+	Password  string             `json:"password" validate:"required"`
 }
 
 type TaskUsecase interface{
 	GetTasks() ([]Task, error)
-	GetTask(id string) (Task, error)
+	GetTaskByID(id string) (Task, error)
 	CreateTask(newTask Task) (Task, error)
 	UpdateTask(id string, updatedTask Task) (Task, error)
 	DeleteTask(id string) error
@@ -38,7 +39,7 @@ type TaskUsecase interface{
 
 type UserUsecase interface{
 	Register(user User) (interface{}, error)
-	Login(username, password string) (string, error)
+	Login(userCredentials UserCredentials) (string, error)
 	PromoteUser(username string) error
 }
 
@@ -56,6 +57,7 @@ type UserRepository interface{
 	PromoteUser(username string, ctx context.Context) error
 	CountUserByUsername(username string, ctx context.Context) (int, error)
 	CountUserByEmail(email string, ctx context.Context) (int, error)
+	CountAllUsers()(int, error)
 	GetUserByUsername(usename string, ctx context.Context) (User, error)
 	
 }
